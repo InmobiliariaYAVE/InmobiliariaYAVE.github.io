b93f0ddd056c20f0721d56521721ef088c12d78c
InmobiliariaYAVE <inmobiliariayave@gmail.com>
2025-11-09 23:12:46 -0600

    Update Desarrollo_Los_Cruz.js

--- Desarrollo_Los_Cruz/Desarrollo_Los_Cruz.js
+++ Desarrollo_Los_Cruz/Desarrollo_Los_Cruz.js
@@ -9,8 +9,8 @@ const terrenos = [
     medidas: "120m²",
     universidad: "Las mejores universidades a solo 10 min",
     precio: 50000,
-    img: "Las_Cruces/img/Las_Cruces (7).jpeg",
-    pagina: "Las_Cruces/Las_Cruces.html"
+    img: "../Las_Cruces/img/Las_Cruces (7).jpeg",
+    pagina: "Las_Cruces/index.html"
   },
   {
     nombre: "Barrio Benito",
@@ -21,7 +21,7 @@ const terrenos = [
     medidas: "Desde 115.50m² hasta 150m²",
     precio: 169500,
     img: "Barrio_Benito/img/Barrio_Benito (1).jpeg",
-    pagina: "Barrio_Benito/Barrio_Benito.html"
+    pagina: "Barrio_Benito/index.html"
   },
   {
     nombre: "Quinta Pascual",
@@ -32,7 +32,7 @@ const terrenos = [
     medidas: "Lotes desde 108m²",
     precio: "1700 el m²",
     img: "Quinta_Pascual/img/Quinta_Pascual (3).jpeg",
-    pagina: "Quinta_Pascual/Quinta_Pascual.html"
+    pagina: "Quinta_Pascual/index.html"
   },
   {
     nombre: "Rancho Santa Julia",
@@ -43,7 +43,7 @@ const terrenos = [
     medidas: "LOTES DE 150m²",
     precio: 150000 ,
     img: "Rancho_Santa_Julia/img/Rancho_Santa_Julia (17).jpeg",
-    pagina: "Rancho_Santa_Julia/Rancho_Santa_Julia.html"
+    pagina: "Rancho_Santa_Julia/index.html"
   }
 ];
 
